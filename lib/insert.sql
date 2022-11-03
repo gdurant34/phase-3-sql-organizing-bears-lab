0f173d4767d830910cd4238cb05333aebfa28bbf
Grace Durant <gdurant34@gmail.com>
2022-11-03 13:39:23 -0700

    commit tree b862c6cbc2c137a1a84fcabb5410df4e8c3918a7

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('MR. CHOCOLATE', 5, 'M', 'brown', 'mild', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 4, 'M', 'brown', 'hot', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 4, 'F', 'gold', 'mild', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 22, 'M', 'brown', 'mild', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 12, 'F', 'brown', 'mild', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 2, 'M', 'green', 'hot', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 8, 'F', 'black', 'mild', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (Null, 4, 'F', 'black', 'mild', 1);
+
